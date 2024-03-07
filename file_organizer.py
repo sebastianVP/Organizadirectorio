6992ec354dd1a26bdb2411081036e95c63955faf
sebastianVP <alex.valdezp22@gmail.com>
2024-03-07 11:25:47 -0500

    fix update

--- file_organizer.py
+++ file_organizer.py
@@ -19,7 +19,7 @@ def directory(file_extension):
         "doc"  : "OfficeWord",
         "docx" : "OfficeWord",
         "xlsx" : "ExcelFiles",
-        "xlsm" : "ExcelFiles","
+        "xlsm" : "ExcelFiles",
         "pdf"  : "PDFDocuments",
         "py"   : "PYTHONScripts",
         "mp3"  : "MUSIC",
